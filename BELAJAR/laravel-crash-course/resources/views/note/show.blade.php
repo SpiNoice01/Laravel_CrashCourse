d54d49caa83f2ea9a9e2cfd9463e4aa0c4b8d676
SpiNoice01 <fadhilaagil72@gmail.com>
2025-05-17 09:21:07 +0700

    DEFINE VIEWS.
    disini kita mengedit html atau blade nya. supaya get data, kemudian menambahkan route kedalam action dan juga href nya

--- BELAJAR/laravel-crash-course/resources/views/note/show.blade.php
+++ BELAJAR/laravel-crash-course/resources/views/note/show.blade.php
@@ -1,3 +1,20 @@
 <x-layout>
-    <h1>Show</h1>
+    <div class="note-container single-note">
+        <div class="note-header">
+            <h1>Note: {{ $note->created_at }}</h1>
+            <div class="note-buttons">
+                <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
+                <button class="note-delete-button">Delete</button>
+            </div>
+        </div>
+        <div class="note">
+            <div class="note-body">
+                {{ $note->note }}
+            </div>
+        </div>
+        <div class="note-buttons">
+            <a href="{{ route('note.index') }}" class="note-cancel-button">Cancel</a> <button
+                class="note-submit-button">Submit</button>
+        </div>
+    </div>
 </x-layout>
